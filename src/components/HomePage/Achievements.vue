596c4a210c5ff912a092593bae5909802a976bdb
cheedinmar <cheedinmarblessing@gmail.com>
2022-05-13 10:31:56 +0100

    fix:css fixes

--- src/components/HomePage/Achievements.vue
+++ src/components/HomePage/Achievements.vue
@@ -4,23 +4,25 @@
       <div
         class="
           items-center
-          space-y-16
-          md:space-y-0    
           h-fit
           flex
           md:flex-row
           flex-col
           mt-10
           bg-gray-50
+          md:mx-8
+          mx-3
+          md:mb-10
+          mb-3
         "
       >
        
-        <div class="w-1/2">
+        <div class="md:w-1/2 w-full">
              <img src="../../assets/images/approve.jpg" class="w-full"/> 
         </div>
-        <div class="text-white flex md:px-10  items-center w-1/2">
+        <div class="text-white flex md:px-10 p-8 md:p-0 items-center md:w-1/2 w-full">
          
-          <p class="md:text-5xl text-4xl text-black tracking-wider font-semibold text-center"><span class="text-brand ">92%</span> of applicants get approved for a loan or products.</p>
+          <p class="md:text-5xl  text-3xl text-black tracking-wider font-semibold text-center"><span class="text-brand ">92%</span> of applicants get approved for a loan or products.</p>
         </div>
       
       </div>
